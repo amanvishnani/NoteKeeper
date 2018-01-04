f4408baabdb50c34c2e0cf05cc178918ad9f999b
Aman Vishnani <aman62@gmail.com>
2018-01-05 00:46:27 +0530

    Added Implicit intents. Replaced parcable objects with primitive type for intents.

--- app/src/main/java/com/amanvishnani/notekeeper/NoteActivity.java
+++ app/src/main/java/com/amanvishnani/notekeeper/NoteActivity.java
@@ -2,11 +2,8 @@ package com.amanvishnani.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -17,9 +14,13 @@ import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
 
-    public static final String NOTE_INFO = "com.amanvishnani.notekeeper.NOTE_INFO";
+    public static final String NOTE_POSITION = "com.amanvishnani.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
     private NoteInfo noteInfo;
     private boolean isNewNote;
+    private Spinner spinnerCourses;
+    private EditText textNoteTitle;
+    private EditText textNoteText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +29,7 @@ public class NoteActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Spinner spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+        spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
         List<CourseInfo> courses = DataManager.getInstance().getCourses();
         ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, courses);
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -36,8 +37,8 @@ public class NoteActivity extends AppCompatActivity {
 
         readDisplayStateValues();
 
-        EditText textNoteTitle = (EditText) findViewById(R.id.text_note_title);
-        EditText textNoteText = (EditText) findViewById(R.id.text_note_text);
+        textNoteTitle = (EditText) findViewById(R.id.text_note_title);
+        textNoteText = (EditText) findViewById(R.id.text_note_text);
 
         if (!isNewNote) {
             displayNote(spinnerCourses, textNoteTitle, textNoteText);
@@ -55,8 +56,11 @@ public class NoteActivity extends AppCompatActivity {
 
     private void readDisplayStateValues() {
         Intent intent = getIntent();
-        noteInfo = intent.getParcelableExtra(NOTE_INFO);
-        isNewNote = noteInfo == null;
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+        isNewNote = position == POSITION_NOT_SET;
+        if(!isNewNote) {
+            noteInfo = DataManager.getInstance().getNotes().get(position);
+        }
     }
 
     @Override
@@ -74,10 +78,24 @@ public class NoteActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_send_mail) {
+            sendEmail();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void sendEmail() {
+        CourseInfo course = (CourseInfo) spinnerCourses.getSelectedItem();
+        String subject = textNoteTitle.getText().toString();
+        String text = "Checkout what I learned in pluralsight course.\""+
+                        course.getTitle()+"\"\n" + textNoteText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+        startActivity(intent);
     }
 }
